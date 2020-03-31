da1442ab6a09bc783c740610f3bb83cfc946fe11
sai218 <kandavalli.saibabu@gmail.com>
2020-03-31 17:58:55 +0530

    vpc tf and jenkinsfile

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
-  region  = "us-east-1"
+  region  = "us-west-2"
+  export AWS_ACCESS_KEY_ID="aws_access_id"
+  export AWS_SECRET_ACCESS_KEY="aws_secret_key"
 }
  resource "aws_vpc" "example" {
    cidr_block = "10.0.0.0/16"
