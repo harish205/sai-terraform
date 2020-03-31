262ade8dbfaba6bf45844206f7c2557c05cd5b41
sai218 <kandavalli.saibabu@gmail.com>
2020-03-31 17:18:15 +0530

    vpc tf

---
+++ Jenkinsfile
@@ -0,0 +1,7 @@
+node{
+// cloning the git repo
+
+stage('scm check out')
+
+
+}
--- main.tf
+++ main.tf
@@ -1,42 +1,9 @@
 provider "aws" {
   region  = "us-east-1"
 }
-# resource "aws_vpc" "example" {
-#   cidr_block = "10.0.0.0/16"
-#   tags = {
-#       Name = "myDevOpsVPC"
-#   }
-# }
-
-data "aws_vpc" "myVPC" {
-  id = "vpc-05cd0243712cebecd"
-}
-
-resource "aws_subnet" "subnet1" {
-  vpc_id     = "${data.aws_vpc.myVPC.id}"
-  cidr_block = "192.168.41.0/24"
-
-  tags = {
-    Name = "pbsubnet-1"
+ resource "aws_vpc" "example" {
+   cidr_block = "10.0.0.0/16"
+   tags = {
+       Name = "myDevOpsVPC"
   }
-}
-
-output "vpc-id" {
-  value = "${data.aws_vpc.myVPC.id}"
-}
-
-output "vpc-cidr" {
-  value = "${data.aws_vpc.myVPC.cidr_block}"
-}
-
-output "vpc-owner" {
-  value = "${data.aws_vpc.myVPC.owner_id}"
-}
-
-output "vpc-tags-env" {
-  value = "${data.aws_vpc.myVPC.tags.Env}"
-}
-
-output "vpc-tags-loc" {
-  value = "${data.aws_vpc.myVPC.tags.Location}"
-}
+ }  
